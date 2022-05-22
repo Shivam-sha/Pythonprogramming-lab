a14f5b3956b8be4a4905100e0537895e951de902
Shivam-sha <97455568+Shivam-sha@users.noreply.github.com>
2022-05-22 11:59:07 +0530

    Add files via upload

---
+++ string3.py
@@ -0,0 +1,12 @@
+# newton first low
+newton = '''Newton first low state that a body at rest or uniform motion will
+            continue to be at rest or uniform motion until the external force is applied it
+            '''
+
+print(len(newton))
+print(newton[26])
+p = (newton[0:25])
+print(p)
+print(newton.endswith("it"))
+print(newton.count("g"))
+print(newton.find("continue"))
